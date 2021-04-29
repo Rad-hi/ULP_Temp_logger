93d069108e9280ac2a1c75b4d10155271a1bfd3b
Rad-hi <radhisghaier33@gmail.com>
2021-04-29 05:41:55 +0100

    Cleanups

--- MQTT.h
+++ MQTT.h
@@ -6,10 +6,11 @@
   
   #define BROKER            "io.adafruit.com"
   #define MQTT_PORT         1883
-  #define MQTT_USER         ""
-  #define MQTT_PASSWORD     ""
-  #define TOPIC             ""
-  #define MQTT_ID           ""
+  #define MQTT_USER         "" // User name in adafruit.io
+  #define MQTT_PASSWORD     "" // MQTT Key in adafruit.io
+  #define TOPIC             "" // User_Name/feeds/Topic_Name_Of_Choice
+  #define MQTT_ID           "" // Create fixed ID for your ESP8266 board
+                               // If connected to the broker once with an ID, never change it!
   
   void MQTT_setup();
   void MQTT_connect();
